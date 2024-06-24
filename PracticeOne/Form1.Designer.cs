9d91d40760d50af2a84577da26cb76e16eef67a6
xom9kxom9k <vfomicev586@gmail.com>
2024-06-24 14:06:45 +0400

    Выполнены все номера кроме 5 и 6

--- PracticeOne/Form1.Designer.cs
+++ PracticeOne/Form1.Designer.cs
@@ -28,12 +28,322 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            panelNumbers = new Panel();
+            buttonSix = new Button();
+            buttonFive = new Button();
+            buttonFour = new Button();
+            buttonThree = new Button();
+            buttonTwo = new Button();
+            buttonOne = new Button();
+            panelArray = new Panel();
+            buttonTwoArray = new Button();
+            buttonOneArray = new Button();
+            textBoxArray = new TextBox();
+            labelMax = new Label();
+            labelMin = new Label();
+            numericUpDownMax = new NumericUpDown();
+            numericUpDownMin = new NumericUpDown();
+            labelCols = new Label();
+            labelRows = new Label();
+            numericUpDownCols = new NumericUpDown();
+            numericUpDownRow = new NumericUpDown();
+            panelResult = new Panel();
+            textBoxResult = new TextBox();
+            buttonStart = new Button();
+            textBoxInfo = new TextBox();
+            panelNumbers.SuspendLayout();
+            panelArray.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownMax).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownMin).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownCols).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownRow).BeginInit();
+            panelResult.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panelNumbers
+            // 
+            panelNumbers.Controls.Add(buttonSix);
+            panelNumbers.Controls.Add(buttonFive);
+            panelNumbers.Controls.Add(buttonFour);
+            panelNumbers.Controls.Add(buttonThree);
+            panelNumbers.Controls.Add(buttonTwo);
+            panelNumbers.Controls.Add(buttonOne);
+            panelNumbers.Dock = DockStyle.Left;
+            panelNumbers.Location = new Point(0, 0);
+            panelNumbers.Name = "panelNumbers";
+            panelNumbers.Size = new Size(196, 450);
+            panelNumbers.TabIndex = 0;
+            // 
+            // buttonSix
+            // 
+            buttonSix.Location = new Point(103, 166);
+            buttonSix.Name = "buttonSix";
+            buttonSix.Size = new Size(75, 23);
+            buttonSix.TabIndex = 5;
+            buttonSix.Text = "Задание 6";
+            buttonSix.UseVisualStyleBackColor = true;
+            buttonSix.Click += buttonSix_Click;
+            // 
+            // buttonFive
+            // 
+            buttonFive.Location = new Point(12, 166);
+            buttonFive.Name = "buttonFive";
+            buttonFive.Size = new Size(75, 23);
+            buttonFive.TabIndex = 4;
+            buttonFive.Text = "Задание 5";
+            buttonFive.UseVisualStyleBackColor = true;
+            buttonFive.Click += buttonFive_Click;
+            // 
+            // buttonFour
+            // 
+            buttonFour.Location = new Point(103, 121);
+            buttonFour.Name = "buttonFour";
+            buttonFour.Size = new Size(75, 23);
+            buttonFour.TabIndex = 3;
+            buttonFour.Text = "Задание 4";
+            buttonFour.UseVisualStyleBackColor = true;
+            buttonFour.Click += buttonFour_Click;
+            // 
+            // buttonThree
+            // 
+            buttonThree.Location = new Point(12, 121);
+            buttonThree.Name = "buttonThree";
+            buttonThree.Size = new Size(75, 23);
+            buttonThree.TabIndex = 2;
+            buttonThree.Text = "Задание 3";
+            buttonThree.UseVisualStyleBackColor = true;
+            buttonThree.Click += buttonThree_Click;
+            // 
+            // buttonTwo
+            // 
+            buttonTwo.Location = new Point(103, 78);
+            buttonTwo.Name = "buttonTwo";
+            buttonTwo.Size = new Size(75, 23);
+            buttonTwo.TabIndex = 1;
+            buttonTwo.Text = "Задание 2";
+            buttonTwo.UseVisualStyleBackColor = true;
+            buttonTwo.Click += buttonTwo_Click;
+            // 
+            // buttonOne
+            // 
+            buttonOne.Location = new Point(12, 78);
+            buttonOne.Name = "buttonOne";
+            buttonOne.Size = new Size(75, 23);
+            buttonOne.TabIndex = 0;
+            buttonOne.Text = "Задание 1";
+            buttonOne.UseVisualStyleBackColor = true;
+            buttonOne.Click += buttonOne_Click;
+            // 
+            // panelArray
+            // 
+            panelArray.Controls.Add(buttonTwoArray);
+            panelArray.Controls.Add(buttonOneArray);
+            panelArray.Controls.Add(textBoxArray);
+            panelArray.Controls.Add(labelMax);
+            panelArray.Controls.Add(labelMin);
+            panelArray.Controls.Add(numericUpDownMax);
+            panelArray.Controls.Add(numericUpDownMin);
+            panelArray.Controls.Add(labelCols);
+            panelArray.Controls.Add(labelRows);
+            panelArray.Controls.Add(numericUpDownCols);
+            panelArray.Controls.Add(numericUpDownRow);
+            panelArray.Location = new Point(428, 0);
+            panelArray.Name = "panelArray";
+            panelArray.Size = new Size(371, 206);
+            panelArray.TabIndex = 1;
+            // 
+            // buttonTwoArray
+            // 
+            buttonTwoArray.Location = new Point(265, 144);
+            buttonTwoArray.Name = "buttonTwoArray";
+            buttonTwoArray.Size = new Size(99, 45);
+            buttonTwoArray.TabIndex = 10;
+            buttonTwoArray.Text = "Двумерный массив";
+            buttonTwoArray.UseVisualStyleBackColor = true;
+            buttonTwoArray.Click += buttonTwoArray_Click;
+            // 
+            // buttonOneArray
+            // 
+            buttonOneArray.Location = new Point(265, 90);
+            buttonOneArray.Name = "buttonOneArray";
+            buttonOneArray.Size = new Size(99, 45);
+            buttonOneArray.TabIndex = 9;
+            buttonOneArray.Text = "Одномерный массив";
+            buttonOneArray.UseVisualStyleBackColor = true;
+            buttonOneArray.Click += buttonOneArray_Click;
+            // 
+            // textBoxArray
+            // 
+            textBoxArray.Location = new Point(18, 90);
+            textBoxArray.Multiline = true;
+            textBoxArray.Name = "textBoxArray";
+            textBoxArray.ScrollBars = ScrollBars.Vertical;
+            textBoxArray.Size = new Size(229, 99);
+            textBoxArray.TabIndex = 8;
+            // 
+            // labelMax
+            // 
+            labelMax.AutoSize = true;
+            labelMax.Location = new Point(203, 48);
+            labelMax.Name = "labelMax";
+            labelMax.Size = new Size(93, 15);
+            labelMax.TabIndex = 7;
+            labelMax.Text = "Макс. значение";
+            // 
+            // labelMin
+            // 
+            labelMin.AutoSize = true;
+            labelMin.Location = new Point(18, 46);
+            labelMin.Name = "labelMin";
+            labelMin.Size = new Size(89, 15);
+            labelMin.TabIndex = 6;
+            labelMin.Text = "Мин. значение";
+            // 
+            // numericUpDownMax
+            // 
+            numericUpDownMax.Location = new Point(302, 46);
+            numericUpDownMax.Maximum = new decimal(new int[] { 1000, 0, 0, 0 });
+            numericUpDownMax.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDownMax.Name = "numericUpDownMax";
+            numericUpDownMax.Size = new Size(62, 23);
+            numericUpDownMax.TabIndex = 5;
+            numericUpDownMax.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDownMax.ValueChanged += numericUpDownMax_ValueChanged;
+            // 
+            // numericUpDownMin
+            // 
+            numericUpDownMin.Location = new Point(128, 46);
+            numericUpDownMin.Maximum = new decimal(new int[] { 1, 0, 0, int.MinValue });
+            numericUpDownMin.Minimum = new decimal(new int[] { 1000, 0, 0, int.MinValue });
+            numericUpDownMin.Name = "numericUpDownMin";
+            numericUpDownMin.Size = new Size(62, 23);
+            numericUpDownMin.TabIndex = 4;
+            numericUpDownMin.Value = new decimal(new int[] { 1, 0, 0, int.MinValue });
+            numericUpDownMin.ValueChanged += numericUpDownMin_ValueChanged;
+            // 
+            // labelCols
+            // 
+            labelCols.AutoSize = true;
+            labelCols.Location = new Point(18, 19);
+            labelCols.Name = "labelCols";
+            labelCols.Size = new Size(104, 15);
+            labelCols.TabIndex = 3;
+            labelCols.Text = "Кол-во столбцов:";
+            // 
+            // labelRows
+            // 
+            labelRows.AutoSize = true;
+            labelRows.Location = new Point(213, 19);
+            labelRows.Name = "labelRows";
+            labelRows.Size = new Size(83, 15);
+            labelRows.TabIndex = 2;
+            labelRows.Text = "Кол-во строк:";
+            // 
+            // numericUpDownCols
+            // 
+            numericUpDownCols.Location = new Point(128, 17);
+            numericUpDownCols.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDownCols.Name = "numericUpDownCols";
+            numericUpDownCols.Size = new Size(38, 23);
+            numericUpDownCols.TabIndex = 1;
+            numericUpDownCols.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDownCols.ValueChanged += numericUpDownCols_ValueChanged;
+            // 
+            // numericUpDownRow
+            // 
+            numericUpDownRow.Location = new Point(302, 17);
+            numericUpDownRow.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDownRow.Name = "numericUpDownRow";
+            numericUpDownRow.Size = new Size(38, 23);
+            numericUpDownRow.TabIndex = 0;
+            numericUpDownRow.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDownRow.ValueChanged += numericUpDownRow_ValueChanged;
+            // 
+            // panelResult
+            // 
+            panelResult.Controls.Add(textBoxResult);
+            panelResult.Controls.Add(buttonStart);
+            panelResult.Controls.Add(textBoxInfo);
+            panelResult.Dock = DockStyle.Bottom;
+            panelResult.Location = new Point(196, 212);
+            panelResult.Name = "panelResult";
+            panelResult.Size = new Size(604, 238);
+            panelResult.TabIndex = 2;
+            // 
+            // textBoxResult
+            // 
+            textBoxResult.Location = new Point(10, 136);
+            textBoxResult.Multiline = true;
+            textBoxResult.Name = "textBoxResult";
+            textBoxResult.ScrollBars = ScrollBars.Vertical;
+            textBoxResult.Size = new Size(586, 99);
+            textBoxResult.TabIndex = 2;
+            // 
+            // buttonStart
+            // 
+            buttonStart.Location = new Point(212, 107);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(186, 23);
+            buttonStart.TabIndex = 1;
+            buttonStart.Text = "Выполнить задание";
+            buttonStart.UseVisualStyleBackColor = true;
+            buttonStart.Click += buttonStart_Click;
+            // 
+            // textBoxInfo
+            // 
+            textBoxInfo.Location = new Point(10, 14);
+            textBoxInfo.Multiline = true;
+            textBoxInfo.Name = "textBoxInfo";
+            textBoxInfo.ScrollBars = ScrollBars.Vertical;
+            textBoxInfo.Size = new Size(586, 87);
+            textBoxInfo.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panelResult);
+            Controls.Add(panelArray);
+            Controls.Add(panelNumbers);
+            Name = "Form1";
+            Text = "Практика 1";
+            panelNumbers.ResumeLayout(false);
+            panelArray.ResumeLayout(false);
+            panelArray.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownMax).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownMin).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownCols).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownRow).EndInit();
+            panelResult.ResumeLayout(false);
+            panelResult.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panelNumbers;
+        private Button buttonSix;
+        private Button buttonFive;
+        private Button buttonFour;
+        private Button buttonThree;
+        private Button buttonTwo;
+        private Button buttonOne;
+        private Panel panelArray;
+        private NumericUpDown numericUpDownCols;
+        private NumericUpDown numericUpDownRow;
+        private Label labelRows;
+        private Label labelCols;
+        private NumericUpDown numericUpDownMax;
+        private NumericUpDown numericUpDownMin;
+        private Label labelMin;
+        private Label labelMax;
+        private TextBox textBoxArray;
+        private Button buttonTwoArray;
+        private Button buttonOneArray;
+        private Panel panelResult;
+        private TextBox textBoxInfo;
+        private Button buttonStart;
+        private TextBox textBoxResult;
     }
 }
--- PracticeOne/Form1.cs
+++ PracticeOne/Form1.cs
@@ -1,10 +1,366 @@
+using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace PracticeOne
 {
     public partial class Form1 : Form
     {
+        private int colsCount = 1;
+        private int rowsCount = 1;
+        private int minValue = -1;
+        private int maxValue = 1;
+        private int[] arrayOne;
+        private int[,] arrayTwo;
+        private int currentTask = 0;
         public Form1()
         {
             InitializeComponent();
+            arrayOne = new int[colsCount];
+            arrayTwo = new int[rowsCount, colsCount];
+        }
+        private void numericUpDownCols_ValueChanged(object sender, EventArgs e)
+        {
+            colsCount = (int)numericUpDownCols.Value;
+            arrayOne = new int[colsCount];
+            arrayTwo = new int[rowsCount, colsCount];
+        }
+        private void numericUpDownRow_ValueChanged(object sender, EventArgs e)
+        {
+            rowsCount = (int)numericUpDownRow.Value;
+            arrayTwo = new int[rowsCount, colsCount];
+        }
+        private void numericUpDownMin_ValueChanged(object sender, EventArgs e)
+        {
+            minValue = (int)numericUpDownMin.Value;
+        }
+        private void numericUpDownMax_ValueChanged(object sender, EventArgs e)
+        {
+            maxValue = (int)numericUpDownMax.Value;
+        }
+
+        private void buttonOne_Click(object sender, EventArgs e)
+        {
+            textBoxInfo.Text = "Дан целочисленный одномерный массив размера N. Серия – это\r\nпоследовательность элементов массива, идущих друг за другом.\r\nКаждый элемент серии больше предыдущего. Серия должна содержать\r\nминимум 2 элемента. Длина серии – количество элементов в серии.\r\nНайти все серии. Вывести информацию с указанием длины для каждой\r\nсерий исходного массива.";
+            currentTask = 1;
+            panelArray.Enabled = true;
+            buttonOneArray.Enabled = true;
+            buttonTwoArray.Enabled = false;
+            numericUpDownRow.Enabled = false;
+            textBoxResult.Clear();
+        }
+
+        private void buttonTwo_Click(object sender, EventArgs e)
+        {
+            textBoxInfo.Text = "Дан целочисленный одномерный массив размера N. Переставить в\r\nобратном порядке элементы массива, расположенные между первым\r\nотрицательным и последним положительным";
+            currentTask = 2;
+            panelArray.Enabled = true;
+            buttonOneArray.Enabled = true;
+            buttonTwoArray.Enabled = false;
+            numericUpDownRow.Enabled = false;
+            textBoxResult.Clear();
+        }
+
+        private void buttonThree_Click(object sender, EventArgs e)
+        {
+            textBoxInfo.Text = "Дан целочисленный одномерный массив размера N. Вставить 3 после\r\nкаждого нечетного элемента массива (нечетный по значению, а не по\r\nпорядковому номеру).";
+            currentTask = 3;
+            panelArray.Enabled = true;
+            buttonOneArray.Enabled = true;
+            buttonTwoArray.Enabled = false;
+            numericUpDownRow.Enabled = false;
+            textBoxResult.Clear();
+        }
+
+        private void buttonFour_Click(object sender, EventArgs e)
+        {
+            textBoxInfo.Text = "Дан целочисленный одномерный массив размера N. Удалить из массива\r\nвсе нечетные элементы (нечетный по значению, а не по порядковому\r\nномеру).\r\n";
+            currentTask = 4;
+            panelArray.Enabled = true;
+            buttonOneArray.Enabled = true;
+            buttonTwoArray.Enabled = false;
+            numericUpDownRow.Enabled = false;
+            textBoxResult.Clear();
+        }
+
+        private void buttonFive_Click(object sender, EventArgs e)
+        {
+            textBoxInfo.Text = "Дан целочисленный двумерный массив размера N*N. Добавить в массив\r\nстроку 0 после каждой строки, в которой есть хотя бы один элемент,\r\nменьший по модулю среднего арифметического элементов массива.\r\n";
+            currentTask = 5;
+            panelArray.Enabled = true;
+            buttonOneArray.Enabled = false;
+            buttonTwoArray.Enabled = true;
+            numericUpDownRow.Enabled = true;
+            textBoxResult.Clear();
+        }
+
+        private void buttonSix_Click(object sender, EventArgs e)
+        {
+            textBoxInfo.Text = "Дан целочисленный двумерный массив размера N*N. Упорядочить\r\nэлементы в матрице по возрастанию (справа налево, снизу-вверх)\r\nПример\r\n1 3 6\r\n4 9 7\r\n0 5 3\r\nпосле сортировки:\r\n9 7 6\r\n5 4 3\r\n3 1 0";
+            currentTask = 6;
+            panelArray.Enabled = true;
+            buttonOneArray.Enabled = false;
+            buttonTwoArray.Enabled = true;
+            numericUpDownRow.Enabled = true;
+            textBoxResult.Clear();
+        }
+
+        private void buttonOneArray_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            if (currentTask == 3)
+            {
+                arrayOne = new int[colsCount * 2];
+            }
+
+            for (int i = 0; i < colsCount; i++)
+            {
+                arrayOne[i] = random.Next(minValue, maxValue);
+            }
+
+            textBoxArray.Text = string.Join(", ", arrayOne);
+
+            buttonStart.Enabled = true;
+        }
+
+        private void buttonTwoArray_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            if (currentTask == 5)
+            {
+                arrayTwo = new int[rowsCount * 2, colsCount];
+
+                for (int i = 0; i < rowsCount; i++)
+                {
+                    for (int j = 0; j < colsCount; j++)
+                    {
+                        arrayTwo[i, j] = rand.Next(minValue, maxValue);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < rowsCount; i++)
+                {
+                    for (int j = 0; j < colsCount; j++)
+                    {
+                        arrayTwo[i, j] = rand.Next(minValue, maxValue);
+                    }
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < rowsCount; i++)
+            {
+                for (int j = 0; j < colsCount; j++)
+                {
+                    sb.Append(arrayTwo[i, j].ToString() + ", ");
+                }
+                sb.AppendLine();
+            }
+            textBoxArray.Text = sb.ToString();
+
+            buttonStart.Enabled = true;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            switch (currentTask)
+            {
+                case 1:
+                    int maxLength = 0;
+                    int maxStartIndex = -1;
+                    int currentStartIndex = 0;
+                    int currentLength = 1;
+
+                    for (int i = 1; i < arrayOne.Length; i++)
+                    {
+                        if (arrayOne[i] > arrayOne[i - 1])
+                        {
+                            currentLength++;
+                        }
+                        else
+                        {
+                            if (currentLength > maxLength)
+                            {
+                                maxLength = currentLength;
+                                maxStartIndex = currentStartIndex;
+                            }
+                            currentStartIndex = i;
+                            currentLength = 1;
+                        }
+                    }
+
+                    if (currentLength > maxLength)
+                    {
+                        maxLength = currentLength;
+                        maxStartIndex = currentStartIndex;
+                    }
+
+                    if (maxLength < 2)
+                    {
+                        textBoxResult.Text = "!Серий не найдено!";
+                    }
+                    else
+                    {
+                        textBoxResult.Text = "Серия начинается с позиции: " + maxStartIndex.ToString() + " \n\n\rЕе длина: " + maxLength.ToString();
+                    }
+
+                    break;
+                case 2:
+                    if (arrayOne.Length < 2)
+                    {
+                        textBoxResult.Text = "!Размер слишком маленький!";
+                        break;
+                    }
+
+                    int negIndex = -1;
+                    int posIndex = -1;
+
+                    for ( int i = 0; i < arrayOne.Length; i ++)
+                    {
+                        if (arrayOne[i] > 0) posIndex = arrayOne[i]; 
+                    }
+                    for (int i = arrayOne.Length - 1; i >=0; i--)
+                    {
+                        if (arrayOne[i] < 0) negIndex = arrayOne[i];
+                    }
+
+                    if (negIndex > posIndex)
+                    {
+                        int temp = negIndex;
+                        negIndex = posIndex;
+                        posIndex = temp;
+                    }
+
+                    while (negIndex < posIndex)
+                    {
+                        int temp = arrayOne[negIndex + 1];
+                        arrayOne[negIndex + 1] = arrayOne[posIndex - 1];
+                        arrayOne[posIndex - 1] = temp;
+                        negIndex++;
+                        posIndex--;
+                    }
+
+                    textBoxResult.Text = string.Join(", ", arrayOne);
+
+                    break;
+                case 3:
+                    int n = arrayOne.Length;
+
+                    for (int i = n - 1; i >= 0; i--)
+                    {
+                        if (arrayOne[i] % 2 != 0 && arrayOne[i] != 0)
+                        {
+                            int m = arrayOne.Length;
+
+                            for (int j = m - 1; j > i + 1; j--)
+                            {
+                                arrayOne[j] = arrayOne[j - 1];
+                            }
+
+                            if (i + 1 < n)
+                            {
+                                arrayOne[i + 1] = 3;
+                            }
+                        }
+                    }
+
+                    textBoxResult.Text = string.Join(", ", arrayOne);
+
+                    break;
+                case 4:
+                    n = arrayOne.Length;
+                    int newLength = n;
+
+                    for (int i = 0; i < newLength; i++)
+                    {
+                        if (arrayOne[i] % 2 != 0)
+                        {
+                            for (int j = i; j < newLength - 1; j++)
+                            {
+                                arrayOne[j] = arrayOne[j + 1];
+                            }
+                            newLength--;
+                            i--;
+                        }
+                    }
+
+                    textBoxResult.Clear();
+                    for (int i = 0; i < newLength; i++)
+                    {
+                        textBoxResult.Text += arrayOne[i].ToString() + ", ";
+                    }
+
+                    break;
+                case 5:
+
+                    /// сделаю 
+                    break;
+                case 6:
+                    rows = array2D.GetLength(0);
+                    cols = array2D.GetLength(1);
+
+                    //Преобразуем в одномерный массив
+                    int[] array = new int[rows * cols];
+                    int index = 0;
+
+                    for (int i = 0; i < rows; i++)
+                    {
+                        for (int j = 0; j < cols; j++)
+                        {
+                            array[index++] = array2D[i, j];
+                        }
+                    }
+
+                    n = array.Length;
+                    bool swapped;
+
+                    do
+                    {
+                        swapped = false;
+                        for (int i = 0; i < n - 1; i++)
+                        {
+                            if (array[i] < array[i + 1])
+                            {
+                                int temp = array[i];
+                                array[i] = array[i + 1];
+                                array[i + 1] = temp;
+                                swapped = true;
+                            }
+                        }
+                        n--;
+                    } while (swapped);
+
+                    index = 0;
+
+                    for (int i = 0; i < rows; i++)
+                    {
+                        for (int j = 0; j < cols; j++)
+                        {
+                            array2D[i, j] = array[index++];
+                        }
+                    }
+
+                    textBoxResult.Clear();
+                    rows = array2D.GetLength(0);
+                    cols = array2D.GetLength(1);
+                    result = "";
+
+                    for (int i = 0; i < rows; i++)
+                    {
+                        for (int j = 0; j < cols; j++)
+                        {
+                            result += array2D[i, j].ToString() + "\t";
+                        }
+                        result += Environment.NewLine;
+                    }
+
+                    textBoxResult.Text = result;
+
+                    break;
+            }
         }
     }
 }
